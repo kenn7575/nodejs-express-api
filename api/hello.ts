29501183c0098e17bd650e23b5e9f117a44fc6cd
Kenni Kollemorten <kenn7575@gmail.com>
2023-06-23 20:05:52 +0200

    weatherData test

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,20 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
+  const RetreiveWeatherData = async (lat, lon) => {
+    var response = await fetch(
+      "https://api.open-meteo.com/v1/forecast?latitude=" +
+        lat +
+        "&longitude=" +
+        lon +
+        "&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,weathercode,uv_index,uv_index_clear_sky&daily=precipitation_probability_mean,weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum&current_weather=true&windspeed_unit=ms&forecast_days=14&timezone=Europe%2FBerlin"
+    );
+    const data = await response.json();
+
+    return data;
+  };
+  const data = RetreiveWeatherData(52.520008, 13.404954);
   return res.json({
-    message: `Hello ${name}!`,
-  })
+    message: `weatherData ${data}!`,
+  });
 }
